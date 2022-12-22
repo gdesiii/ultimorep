b09d943adbb94ca5336aadebfdfa32f9ae7d448a
desig <desig@gdesipc>
2022-12-22 12:00:27 +0100

    aggiunta del live script e delle funzioni necessarie da utilizzare

---
+++ data_normalization.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = data_normalization(inputArg1,inputArg2)
+%DATA_NORMALIZATION Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
---
+++ my_permutation_test.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = my_permtest(inputArg1,inputArg2)
+%MY_PERMTEST Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
