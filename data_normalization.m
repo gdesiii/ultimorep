bf65daf925e0ce30ab28e13e259081214a414a37
Hp <Hp@192.168.1.18>
2022-12-29 13:36:21 +0100

    punto2 completato

--- data_normalization.m
+++ data_normalization.m
@@ -1,7 +1,6 @@
-function [output] = data_normalization(data)
+function [output1,output2] = data_normalization(data)
 %DATA_NORMALIZATION Summary of this function goes here
 %   Detailed explanation goes here
-index=1;
 for index=1 : 1 : 8
     data_matrix=cell2mat(data(index,1));
     if index==1
@@ -12,26 +11,16 @@ for index=1 : 1 : 8
         vector_muscle1=[vector_muscle1;data_matrix(:,2)];
         vector_muscle2=[vector_muscle2;data_matrix(:,3)];
     end
-end
+end 
 max_value1=max(vector_muscle1);
 max_value2=max(vector_muscle2);
-vector_tot=[vector_muscle1;vector_muscle2];
-max_value_tot=max(vector_tot);
-   
-for index=1 : 1 : 8
-    data_matrix=cell2mat(data(index,1));
-    data_matrix_m1=data_matrix(:,2)/max_value_tot;
-    data_matrix_m2=data_matrix(:,3)/max_value_tot;
-    %matrice con tempo + dati normalizzati
-    matrix=[data_matrix(:,1);data_matrix_m1;data_matrix_m2];
-  %completare il passaggio in out
-    if index==1
-        %cell=mat2cell(matrix);
-    end
-    if index>1
-        %cell=[mat2cell(,);
-    end
-end
-output=cell;
+norm_muscle1=vector_muscle1/max_value1;
+norm_muscle2=vector_muscle2/max_value2;
+
+cell_norm_muscle1=mat2cell(norm_muscle1, [length(cell2mat(data(1,1))) length(cell2mat(data(2,1))) length(cell2mat(data(3,1))) length(cell2mat(data(4,1))) length(cell2mat(data(5,1))) length(cell2mat(data(6,1))) length(cell2mat(data(7,1))) length(cell2mat(data(8,1)))] ,[1] );
+cell_norm_muscle2=mat2cell(norm_muscle2, [length(cell2mat(data(1,1))) length(cell2mat(data(2,1))) length(cell2mat(data(3,1))) length(cell2mat(data(4,1))) length(cell2mat(data(5,1))) length(cell2mat(data(6,1))) length(cell2mat(data(7,1))) length(cell2mat(data(8,1)))] ,[1] );
+
+output1=cell_norm_muscle1;
+output2=cell_norm_muscle2;
 end
 
