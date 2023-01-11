ca1190494c6294ee68278ab15936d682a8812e67
arian <arian@LAPTOP-9CJ5093N.homenet.telecomitalia.it>
2023-01-11 22:22:20 +0100

    -creata la funzione dell'obiettivo 1 (bisogna sistemare i titoli dei subplot) -modificata quella dell'obiettivo 2 per usare meglio quello che esce dalla prima funzione e sfruttare di più le operazioni tra matrici -si potrebbe aggiungere una funzione poi per plottare tutti i casi dei soggetti insieme dato che poi bisognerà farlo più volte FINE :)

--- data_normalization.m
+++ data_normalization.m
@@ -1,30 +1,33 @@
-function [output1,output2,output3] = data_normalization(data)
-%DATA_NORMALIZATION Summary of this function goes here
-%   Detailed explanation goes here
-for index=1 : 1 : 8
-    data_matrix=cell2mat(data(index,1));
-    if index==1
-        vector_muscle1=[data_matrix(:,2)];
-        vector_muscle2=[data_matrix(:,3)];
-        time_vector=[data_matrix(:,1)];
-    end
-    if index>1
-        vector_muscle1=[vector_muscle1;data_matrix(:,2)];
-        vector_muscle2=[vector_muscle2;data_matrix(:,3)];
-        time_vector=[time_vector; data_matrix(:,1)];
-    end
-end 
-max_value1=max(vector_muscle1);
-max_value2=max(vector_muscle2);
-norm_muscle1=vector_muscle1/max_value1;
-norm_muscle2=vector_muscle2/max_value2;
+function output = data_normalization(data)
 
-cell_norm_muscle1=mat2cell(norm_muscle1, [length(cell2mat(data(1,1))) length(cell2mat(data(2,1))) length(cell2mat(data(3,1))) length(cell2mat(data(4,1))) length(cell2mat(data(5,1))) length(cell2mat(data(6,1))) length(cell2mat(data(7,1))) length(cell2mat(data(8,1)))] ,[1] );
-cell_norm_muscle2=mat2cell(norm_muscle2, [length(cell2mat(data(1,1))) length(cell2mat(data(2,1))) length(cell2mat(data(3,1))) length(cell2mat(data(4,1))) length(cell2mat(data(5,1))) length(cell2mat(data(6,1))) length(cell2mat(data(7,1))) length(cell2mat(data(8,1)))] ,[1] );
-cell_time=mat2cell(time_vector,[length(cell2mat(data(1,1))) length(cell2mat(data(2,1))) length(cell2mat(data(3,1))) length(cell2mat(data(4,1))) length(cell2mat(data(5,1))) length(cell2mat(data(6,1))) length(cell2mat(data(7,1))) length(cell2mat(data(8,1)))] ,[1]  );
+[num_casi, num_soggetti]= size (data);
+%prealloco matrice 3D
 
-output1=cell_norm_muscle1;
-output2=cell_norm_muscle2;
-output3=cell_time;
+
+for index_soggetti=1:num_soggetti
+
+    %vettore che contiene le dimensioni di ogni caso di ogni paziente
+    for index_casi=1:num_casi
+        dim(index_casi)= length (cell2mat(data (index_casi,index_soggetti)));
+    end
+    
+    %vettore che contiene concatenati tutti i valori dei muscoli
+    muscles= cell2mat(data(:,index_soggetti));
+    max_values = max (muscles);
+    %normalizzo
+    norm = muscles ./ max_values;
+    %creo cell array con le separando i casi grazie al vettore dim
+    %contenente le dimensioni di ognuno
+    cell{index_soggetti}=mat2cell(norm, dim);
+end
+
+
+%QUESTO BRUTTO DA VEDERE QUINDI DA CAMBIARE MA NON AVEVO PIU' VOGLIA
+for index_soggetti=1:num_soggetti
+for index_casi=1:num_casi
+    output{index_casi,index_soggetti}=cell{index_soggetti}(index_casi);
+end
+end
+
 end
 
--- get_envelope.m
+++ get_envelope.m
@@ -1,7 +1,94 @@
-function [outputArg1,outputArg2] = get_envelope(inputArg1,inputArg2)
-%GET_ENVELOPE Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function envelops = get_envelope(data, fs, nfft)
+
+%filtro passa banda
+%rettifico
+%filtro passa basso
+
+%ho 3 soggetti
+%ognisoggetto ha 8 casi
+%ogni caso ha 2 muscoli
+
+% data è il cell array
+
+[num_casi, num_soggetti]= size (data);
+
+% prealloco il cell arrray envelops che conterrà per ogni soggetto
+% l'inviluppo di ogni segnale
+envelops = cell (num_casi, num_soggetti);
+
+for index_soggetto = 1: num_soggetti
+for index_caso = 1: num_casi
+    
+    signal = cell2mat(data(index_caso, index_soggetto));
+    
+    %signal(:,1) = asse tempo
+    %signal(:,2) = muscolo 1 
+    %signal(:,3) = muscolo 2
+    
+    %separo l'asse temporale dai dati dei due muscoli
+    time = signal(:,1);
+    muscles = [signal(:,2) , signal(:,3)];
+    
+ %filtro passa banda
+    yF= fft (muscles, nfft);
+    f=linspace(0, fs/2, floor(nfft/2)+1);
+    
+%     %PLOT DEL MODULO DELLA TRASFORMATA
+%     figure 
+%     plot(f,abs(yF1(1:floor(nfft/2)+1)))
+    
+    %provo a considerare il secondo picco
+    [~, xpeak]= findpeaks (abs(yF(1:floor(nfft/2)+1)));
+    wn= [xpeak(2) - floor(xpeak(2)/2) , 100];
+    %mi servono le frequenze normalizzate per usare fir1
+    wn = wn / (fs/2);
+    
+    %mi aspetto solo coeff. b
+    order=200;
+    [b,a] = fir1(order,wn);
+
+    sig_filtered = filter(b, a, muscles);
+    
+ %rettifica
+    sig_rec= abs (sig_filtered);
+    
+ %filtro passa basso
+
+    yF1= fft (sig_rec, nfft);
+    f=linspace(0, fs/2, floor(nfft/2)+1);
+    %PLOT DEL MODULO DELLA TRASFORMATA
+    figure 
+    plot(f,abs(yF1(1:floor(nfft/2)+1)))
+    %frequenza limite normalizzata
+    wn = 100 / (fs/2);
+
+    %mi aspetto solo coeff. b
+    order=200;
+    [b,a] = fir1(order,wn);
+
+    sig_low = filter(b, a, sig_rec);
+    
+    %inviluppo come valore assoluto del segnale filtrato
+    single_envelop = abs (sig_low);
+    
+    %PLOT confronto tra segnale originale e segnale filtrato
+    
+    subplot (2,1,1)
+    sgtitle(strcat ('plot inviluppo soggetto ', index_soggetto, ' caso ' ,index_caso ) );
+    plot(time, muscles(:,1),"Color","k")
+    hold on
+    plot(time, single_envelop(:,1),"Color", "r")
+    
+    subplot (2,1,2)
+    plot(time, muscles(:,2),"Color","k")
+    hold on
+    plot(time, single_envelop(:,2),"Color", "r")
+    
+    %riempio il cell array
+    envelops {index_caso,index_soggetto} = single_envelop;
+end
+end
+
+
 end
 
