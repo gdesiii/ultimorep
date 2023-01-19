70c67b5a2d1fbb42c76027b2e342558f714677d8
arian <arian@LAPTOP-9CJ5093N.homenet.telecomitalia.it>
2023-01-19 16:57:48 +0100

    permutation

--- my_permutation_test.m
+++ my_permutation_test.m
@@ -1,7 +1,29 @@
-function [outputArg1,outputArg2] = my_permtest(inputArg1,inputArg2)
-%MY_PERMTEST Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function p_value = my_permutation_test(a, b, N)
+  
+[row, ~]=size(a);
+for index=1:row
+Tobs = abs(mean(a(index,:)) - mean(b(index,:)));
+    
+%     nA = numel(a);
+%     nB = numel(b);
+%     lenght(nA+nB);
+   
+    pool=[a(index,:),b(index,:)];
+    
+    Trand=zeros(1,N);
+    for index=1:N
+        pool=pool(randperm(length(pool)));
+        a1=pool(1:length(a));
+        b1=pool(end-length(b): end);
+        Trand(index) = abs(mean(a1) - mean(b1));
+    end
+    
+    p_value = sum (gt (Trand,Tobs))/N;
+    
+    figure
+    histogram(Trand)
+    xline(Tobs)
+end
+     %pvalue = mean(Trand > Tobs)
 end
 
